a34f818306753b244dba5ff0479d5f48b6521da8
Hoppy <dev.hoppy@gmail.com>
2024-05-29 19:55:01 +0200

    Add get route and graceful shutdow

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,44 @@
-use std::fs;
-use std::io;
-use std::path::Path;
 use futures::TryStreamExt;
+use log::*;
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
+use uuid::Uuid;
 use warp::{
-    reply, Reply, Filter, reject,
-    Rejection,
+    filters::multipart::{FormData, Part},
     http::StatusCode,
-    filters::multipart::{FormData, Part}
+    reply, serve, Buf, Filter, Rejection, Reply,
 };
 
 #[tokio::main]
-async fn main() {
-    let upload_route = warp::post()
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    pretty_env_logger::formatted_timed_builder();
+
+    let port = std::env::args()
+        .nth(1)
+        .map(|val| val.parse::<u16>())
+        .unwrap_or(Ok(8080))?;
+
+    let upload_file_route = warp::post()
         .and(warp::path("upload"))
         .and(warp::multipart::form().max_length(5_000_000))
         .and_then(upload);
 
-    let routes = upload_route.recover(handle_rejection);
+    let get_file_route = warp::path!("file" / String).and_then(get_file);
 
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    let routes = upload_file_route
+        .or(get_file_route)
+        .recover(handle_rejection);
+
+    let (_addr, server) = serve(routes).bind_with_graceful_shutdown(([0, 0, 0, 0], port), async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("http_server: Failed to listen for CRTL+c");
+        info!("Shutting down HTTP server");
+    });
+
+    server.await;
+
+    Ok(())
 }
 
 async fn upload(form: FormData) -> Result<impl Reply, Rejection> {
@@ -53,6 +73,8 @@ async fn upload(form: FormData) -> Result<impl Reply, Rejection> {
 
     Ok(id.to_string())
 }
+
+async fn get_file(file_name: String) -> Result<impl Reply, Rejection> {
     Ok("success")
 }
 
@@ -81,6 +103,9 @@ async fn handle_rejection(err: Rejection) -> Result<impl Reply, std::convert::In
         Ok(reply::with_status("BAD_REQUEST", StatusCode::BAD_REQUEST))
     } else {
         eprintln!("unhandled rejection: {:?}", err);
-        Ok(reply::with_status("INTERNAL_SERVER_ERROR", StatusCode::INTERNAL_SERVER_ERROR))
+        Ok(reply::with_status(
+            "INTERNAL_SERVER_ERROR",
+            StatusCode::INTERNAL_SERVER_ERROR,
+        ))
     }
 }

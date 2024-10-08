d2dc4f8e7eca808a20a1c944ee7d786bb0c1a060
Hoppy <dev.hoppy@gmail.com>
2024-10-08 18:25:12 +0200

    Revert "Clean sub folder too"
    This reverts commit e133cee51b9f9a7ac6586e32e2591503b9bba60e.

--- src/schedule.rs
+++ src/schedule.rs
@@ -44,9 +44,6 @@ fn get_files(upload_dir: String) -> Vec<(File, PathBuf)> {
 					if let Ok(file) = File::open(&path) {
 						files.push((file, path));
 					}
-				} else if path.is_dir() {
-					let sub_files = get_files(path.to_string_lossy().into_owned());
-					files.extend(sub_files);
 				}
 			}
 		}
